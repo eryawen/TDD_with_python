153a55edd7d55388645dcf9c0d2a3bca5ed06e91
Monika Kołtunik <monikakoltunik@gmail.com>
2017-05-05 11:27:40 +0200

    Functional tests now checks we can input a to-do item

--- TDD_with_python/functional_tests.py
+++ TDD_with_python/functional_tests.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 import unittest
 
+from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(unittest.TestCase): #inheritance
+
+class NewVisitorTest(unittest.TestCase):  # inheritance
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -12,27 +14,40 @@ class NewVisitorTest(unittest.TestCase): #inheritance
 
     # Any method whose name starts with test_ is a test method
     def test_can_start_a_list_and_retrieve_it_later(self):
-
         # Edith checks out homepage
         # browser.get('http://localhost:8080')
         self.browser.get('http://127.0.0.1:8000/')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do',self.browser.title)
-        self.fail('Finish the test!') # reminds to finish the test
         # assert 'To-Do' in self.browser.title, "Browser title was " + self.browser.title
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a to-do item'
+                         )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
 
+        self.fail('Finish the test!')
         # The page updates again, and now shows both items on her list
 
         # Edith wonders whether the site will remember her list. Then she sees
@@ -43,6 +58,7 @@ class NewVisitorTest(unittest.TestCase): #inheritance
 
         # Satisfied, she goes back to sleep
 
-#checks if script was executed from cmd rather than imported by another script
+
+# checks if script was executed from cmd rather than imported by another script
 if __name__ == '__main__':
-    unittest.main(warnings='ignore') #launches unittest test runner ; warnings?
+    unittest.main(warnings='ignore')  # launches unittest test runner ; warnings?
