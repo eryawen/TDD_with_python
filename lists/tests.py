99411ee85134351696a187777902e1720bcc97b7
Monika Kołtunik <monikakoltunik@gmail.com>
2017-05-06 23:12:45 +0200

    Adds tests checking if the user's input can be saved and displayed in the table

--- lists/tests.py
+++ lists/tests.py
@@ -13,13 +13,24 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(),
-                         expected_html)  # decode converts response bytes into unicode string, previously we compared bytes
-        # self.assertIn(b'<title>To-Do lists</title>', response.content)
-        # self.assertTrue(response.content.endswith(b'</html>'))
-        # self.assertTrue(response.content.strip().endswith(b'</html>')) (if problem with
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
+        self.assertEqual(response.content.decode(), expected_html)
+
+
+# decode converts response bytes into unicode string, previously we compared bytes
+# self.assertIn(b'<title>To-Do lists</title>', response.content)
+# self.assertTrue(response.content.endswith(b'</html>'))
+# self.assertTrue(response.content.strip().endswith(b'</html>')) (if problem with
+
+def test_home_page_can_save_a_POST_request(self):
+    request = HttpRequest()
+    request.method = 'POST'
+    request.POST["item_text"] = 'A new list item'
+
+    response = home_page(request)
+
+    self.assertIn('A new list item', response.content.decode())  # setup, exercise, assert
 
 # class Smoke(TestCase):
 #     def test_bad_maths(self):
--- lists/views.py
+++ lists/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 
 def home_page(request):
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
     return render(request,'home.html') #use django render function instead of creating own HttpResponse
 
